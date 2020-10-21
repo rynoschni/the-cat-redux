c64a348d4dafe3ae5958e59530856a1882f9f849
Ryan Schniederjan <ryno@schnietke.com>
2020-10-21 12:29:58 -0500

    Exercise-1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Activity from './components/Activity';
+
+function App() {
+  return (
+    <div className="App">
+      <Activity />
+    </div>
+  );
+}
+
+export default App;
